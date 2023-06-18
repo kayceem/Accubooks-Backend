3b236eee9f850fc1941f436d527bb6a9bfa15f9b
Kayc <ghstkc@gmail.com>
2023-06-18 18:44:37 +0545

    Add basic profile

--- app/routes/profile.py
+++ app/routes/profile.py
@@ -1,3 +1,17 @@
 from flask.views import MethodView
-from .. import schemas
-from ..schemas import ValidationError
+from flask import request, render_template, redirect, url_for
+from flask_login import login_required, current_user
+from ..database import get_db
+from .. import models, schemas
+from app import bcrypt
+
+class ProfileView(MethodView):
+    decorators = [login_required]
+
+    def get(self):
+        db = get_db()
+        user = db.session.query(models.User).filter(models.User.id==current_user.id).first()
+        return render_template('profile.html', user=user)
+
+    def post(self):
+        pass
